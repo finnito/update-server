6908125488eecddae468647dfb2658717de22803
Finn LeSueur <finn@lesueur.nz>
2021-03-09 15:03:17 +1300

    Fixes colours

--- upgrade.sh
+++ upgrade.sh
@@ -6,8 +6,23 @@ if (( $EUID != 0 )); then
     exit
 fi
 
+echo -e "\e[32m apt update \e[0m"
 apt update
+echo -e "\n"
+
+echo -e "\e[32m apt -y dist-upgrade \e[0m"
 apt -y dist-upgrade
+echo -e "\n"
+
+echo -e "\e[32m apt -y autoremote \e[0m"
 apt -y autoremove
+echo -e "\n"
+
+echo -e "\e[32m apt clean \e[0m"
 apt clean
+echo -e "\n"
+
+echo -e "\e[32m apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }') \e[0m"
 apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
+echo -e "\n"
+
