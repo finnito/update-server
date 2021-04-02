cc392917dfc659ddaba75538b215a8c5b46fb85a
Finn Le Sueur <finn@lesueur.nz>
2021-04-02 13:33:04 +1300

    Creates mailserver upgrade script

---
+++ upgrade-mailserver.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# if not root, run as root
+if (( $EUID != 0 )); then
+    sudo ~/upgrade-server/upgrade.sh
+    exit
+fi
+
+echo -e "\e[32m apt update \e[0m"
+apt update
+echo -e "\n"
+
+echo -e "\e[32m apt -y dist-upgrade \e[0m"
+apt -y dist-upgrade
+echo -e "\n"
+
+echo -e "\e[32m apt -y autoremote \e[0m"
+apt -y autoremove
+echo -e "\n"
+
+echo -e "\e[32m apt clean \e[0m"
+apt clean
+echo -e "\n"
+
+echo -e "\e[32m apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }') \e[0m"
+apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
+echo -e "\n"
+
+curl -s https://mailinabox.email/setup.sh | sudo bash
+
+reboot
