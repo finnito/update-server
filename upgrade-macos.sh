7385cc98bb5515184910785b8c63ddc4e155462f
Finn Le Sueur <finn@lesueur.nz>
2021-12-01 03:00:44 +1300

    Improve todo output

--- upgrade-macos.sh
+++ upgrade-macos.sh
@@ -10,25 +10,31 @@ echo -e " 6. brew doctor"
 echo -e " 7. ?softwareupdate"
 echo -e " 8. ?reboot\e[0m"
 
-echo -e "\n\e[32m1/8 brew update \e[0m"
+echo -n "Proceed? (y/n) "
+read yesno < /dev/tty
+if [ "$yesno" = "n" ];then
+    exit 1
+fi
+
+echo -e "\n\e[32m1/8: brew update \e[0m"
 brew update
 
-echo -e "\n\e[32m2/8 brew outdated \e[0m"
+echo -e "\n\e[32m2/8: brew outdated \e[0m"
 brew outdated
 
-echo -e "\n\e[32m3/8 brew upgrade \e[0m"
+echo -e "\n\e[32m3/8: brew upgrade \e[0m"
 brew upgrade
 
-echo -e "\n\e[32m4/8 brew doctor \e[0m"
+echo -e "\n\e[32m4/8: brew doctor \e[0m"
 brew doctor
 
-echo -e "\n\e[32m5/8 brew cleanup \e[0m"
+echo -e "\n\e[32m5/8: brew cleanup \e[0m"
 brew cleanup
 
-echo -e "\n\e[32m6/8 brew doctor \e[0m"
+echo -e "\n\e[32m6/8: brew doctor \e[0m"
 brew doctor
 
-echo -e "\n\e[32m7/8 sudo softwareupdate --list \e[0m"
+echo -e "\n\e[32m7/8: sudo softwareupdate --list \e[0m"
 sudo softwareupdate --list
 
 echo -n "\nPerform System Update (y/n) "
--- upgrade-webserver.sh
+++ upgrade-webserver.sh
@@ -2,35 +2,50 @@
 
 # if not root, run as root
 if (( $EUID != 0 )); then
-    sudo ~/upgrade-server/upgrade.sh
+    sudo ~/upgrade-server/upgrade-webserver.sh
     exit
 fi
 
-echo -e "\e[32m apt update \e[0m"
+echo -e "\e[32mTODO:"
+echo -e "1. apt update"
+echo -e "2. apt -y dist-upgrade"
+echo -e "3. apt -y autoremove"
+echo -e "4. apt clean"
+echo -e "5. apt purge -y"
+echo -e "6. do-release-upgrade"
+echo -e "7. reboot?\e[0m"
+
+echo -n "Proceed? (y/n) "
+read yesno < /dev/tty
+if [ "$yesno" = "n" ];then
+    exit 1
+fi
+
+echo -e "\e[32m1/7: apt update \e[0m"
 apt update
 echo -e "\n"
 
-echo -e "\e[32m apt -y dist-upgrade \e[0m"
+echo -e "\e[32m2/7: apt -y dist-upgrade \e[0m"
 apt -y dist-upgrade
 echo -e "\n"
 
-echo -e "\e[32m apt -y autoremove \e[0m"
+echo -e "\e[32m3/7: apt -y autoremove \e[0m"
 apt -y autoremove
 echo -e "\n"
 
-echo -e "\e[32m apt clean \e[0m"
+echo -e "\e[32m4/7: apt clean \e[0m"
 apt clean
 echo -e "\n"
 
-echo -e "\e[32m apt purge -y \e[0m"
+echo -e "\e[32m5/7: apt purge -y \e[0m"
 apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
 echo -e "\n"
 
-echo -e "\e[32m do-release-upgrade \e[0m"
+echo -e "\e[32m6/7: do-release-upgrade \e[0m"
 do-release-upgrade
 echo -e "\n"
 
-echo -n "Reboot? (y/n) "
+echo -n "7/7: Reboot? (y/n) "
 read yesno < /dev/tty
 
 if [ "$yesno" = "y" ];then
