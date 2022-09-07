b2e67b5dc54c8b916cc6819a57af175982fbefe8
Finn LeSueur <finn@lesueur.nz>
2022-09-07 12:42:49 +1200

    Improve output

--- upgrade-webserver.sh
+++ upgrade-webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -uo pipefail
 
 # if not root, run as root
 if (( $EUID != 0 )); then
@@ -9,13 +9,15 @@ if (( $EUID != 0 )); then
 fi
 
 echo -e "\e[32mTODO:"
-echo -e "1. apt update"
-echo -e "2. apt -y dist-upgrade"
-echo -e "3. apt -y autoremove"
-echo -e "4. apt clean"
-echo -e "5. apt purge -y"
-echo -e "6. do-release-upgrade"
-echo -e "7. reboot?\e[0m"
+echo -e "1. composer clear-cache"
+echo -e "2. journalctl --vacuum-time=7d"
+echo -e "3. apt update"
+echo -e "4. apt -y dist-upgrade"
+echo -e "5. apt -y autoremove"
+echo -e "6. apt clean"
+echo -e "7. apt purge -y"
+echo -e "8. do-release-upgrade"
+echo -e "9. reboot?\e[0m\n"
 
 echo -n "Proceed? (y/n) "
 read yesno < /dev/tty
@@ -23,40 +25,40 @@ if [ "$yesno" = "n" ];then
     exit 1
 fi
 
-pip cache purge
-sudo -u finn pip cache purge
-
-composer clear-cache
+echo -e "\e[32m1/9: composer clear-cache \e[0m"
 sudo -u finn composer clear-cache
 sudo -u www-data composer clear-cache
+echo -e "\n"
 
+echo -e "\e[32m2/9: journalctl --vacuum-time=7d \e[0m"
 journalctl --vacuum-time=7d
+echo -e "\n"
 
-echo -e "\e[32m1/7: apt update \e[0m"
+echo -e "\e[32m3/9: apt update \e[0m"
 apt update
 echo -e "\n"
 
-echo -e "\e[32m2/7: apt -y dist-upgrade \e[0m"
+echo -e "\e[32m4/9: apt -y dist-upgrade \e[0m"
 apt -y dist-upgrade
 echo -e "\n"
 
-echo -e "\e[32m3/7: apt -y autoremove \e[0m"
+echo -e "\e[32m5/9: apt -y autoremove \e[0m"
 apt -y autoremove
 echo -e "\n"
 
-echo -e "\e[32m4/7: apt clean \e[0m"
+echo -e "\e[32m6/9: apt clean \e[0m"
 apt clean
 echo -e "\n"
 
-echo -e "\e[32m5/7: apt purge -y \e[0m"
+echo -e "\e[32m7/9: apt purge -y \e[0m"
 apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
 echo -e "\n"
 
-echo -e "\e[32m6/7: do-release-upgrade \e[0m"
+echo -e "\e[32m8/9: do-release-upgrade \e[0m"
 do-release-upgrade
 echo -e "\n"
 
-echo -n "7/7: Reboot? (y/n) "
+echo -e "\e[32m9/9: Reboot? (y/n): \e[0m"
 read yesno < /dev/tty
 
 if [ "$yesno" = "y" ];then
